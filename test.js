9fd2e70b834269c2b4dee0300cfe2075fe8524dd
67160179 <67160179@go.buu.ac.th>
2026-01-13 01:48:34 +0700

    remove test.js

--- test.js
+++
@@ -1,4 +0,0 @@
-const firstName = "John";
-const fullName = `${firstName} Doe`; // Template Literal
-console.log("'5' + 2:", "5" + 2); // ผลลัพธ์: "52" (String Concat)
-console.log("'5' - 2:", "5" - 2); // ผลลัพธ์: 3 (Numeric)
